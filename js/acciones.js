637a684e35c7a39a28ae47acc51585d62c426b48
Luis Antonio Gomez Soriano <luis.antonio.gmz@gmail.com>
2013-06-13 07:56:40 -0500

    Version Alpha

---
+++ js/acciones.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+ function LoginConectar(U,P)
+   {
+	   datos="usuario="+U+"&password="+P;
+	   
+	   $.ajax({
+		   type:"POST",
+		   url:"198.168.1.182/sitio6/Log_in_m.php",
+		   data: datos
+		   }).done(function(msg){
+			   alert(msg);
+			   if (msg==""|| msg==null)
+			   {
+				   alert("usuario incorrecto");
+			   }//if
+				   else
+				   {
+					   alert("Bienvenido");
+				   }//else
+});
+   }
+
+
+$(document).ready(function(e) 
+{
+	document.addEventListener("deviceready",function()
+	{
+		$('.Enviar').tap(function()
+		{
+			var formulario=$(this).parents('form');
+			var usuario= document.getElementById('Usuario').value;
+   			var password= document.getElementById('Password').value;
+    });//tap
+	});//deviceredy
+	 });//ready
